e1bcd8c91c4af965f346126ad5135cb5b6c52c17
Simone Morella <simone.morella@ah.nl>
2025-06-01 21:25:11 +0200

    feat: added extra page and finished favorites add and remove

--- lib/main.dart
+++ lib/main.dart
@@ -34,61 +34,165 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
+  void toggleFavorite(WordPair pair) {
+    var word = pair;
+    if (favorites.contains(word)) {
+      favorites.remove(word);
     } else {
-      favorites.add(current);
+      favorites.add(word);
     }
     notifyListeners();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: ChildPage(index: selectedIndex),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class ChildPage extends StatelessWidget {
+  final int index;
+
+  const ChildPage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    if (index == 0) {
+      return GeneratorPage();
+    } else if (index == 1) {
+      return FavoritesPage();
+    } else {
+      return Center(child: Text('Unknown page'));
+    }
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favorites = appState.favorites;
+
+    if (favorites.isEmpty) {
+      return Center(child: Text('No favorites yet!'));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: ListView(
+          children: [
+            Text('There are ${favorites.length} word${favorites.length > 1 ? 's': ''} in your favorites.'),
+            for (var word in favorites)
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text(word.asCamelCase, selectionColor: Theme.of(context).colorScheme.onPrimary),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    appState.toggleFavorite(word);
+                  },
+                ),
+              )
+
+          ]
+
+      ),
+    );
+  }
+}
+
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
-    IconData icon;
 
+    IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Button pressed vamo raga forza juventus');
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                )
-              ],
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite(pair);
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
+// ...
 
 class BigCard extends StatelessWidget {
   const BigCard({
